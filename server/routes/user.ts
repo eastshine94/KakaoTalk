e6a0ba232e54eb0ef264735dba6b53cadeee97ee
KimHeeDong <eastshine94@naver.com>
2020-08-12 16:23:31 +0900

    cannot set headers after they are sent to the client 문제 해결

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -7,20 +7,27 @@ const router = express.Router();
 
 router.post("/find", async(req, res) => {
   const { user_id } = req.body;
-  const user = await User.findOne({
-    attributes: ["id", "user_id", "name", "status_msg", "profile_img_url", "background_img_url"],
-    where: { user_id },
-  });
-  if (user) {
-    return res.json({ 
-      data: user,
-      msg: "이미 사용중이거나 탈퇴한 아이디입니다." 
+  try {
+    const user = await User.findOne({
+      attributes: ["id", "user_id", "name", "status_msg", "profile_img_url", "background_img_url"],
+      where: { user_id },
     });
+    if (user) {
+      return res.json({ 
+        data: user,
+        msg: "이미 사용중이거나 탈퇴한 아이디입니다." 
+      });
+    }
+    return res.json({
+      data: null,
+      msg: "사용 가능한 ID 입니다.",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      msg: "서버 문제로 인해 찾을 수 없습니다."
+    })
   }
-  return res.json({
-    data: null,
-    msg: "사용 가능한 ID 입니다.",
-  });
+  
 });
 
 router.post("/profile/change", async(req, res) => {
