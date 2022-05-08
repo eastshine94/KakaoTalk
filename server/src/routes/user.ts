a15595bd4273df3d63038c2f0a36f767c5db8f2c
hdkim <eastshine94@naver.com>
2022-05-08 23:22:45 +0900

    edit README.md

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,76 +1,92 @@
-import * as express from "express";
-import User from "../models/User";
+import * as express from 'express';
+import User from '../models/User';
 import * as multer from 'multer';
 import * as path from 'path';
 
 const dest = path.join(__dirname, '../uploads/');
-const upload = multer({dest})
+const upload = multer({ dest });
 const router = express.Router();
 
 // 회원가입한 user id로 유저 정보를 찾습니다.
-router.get("/:user_id", async(req, res) => {
+router.get('/:user_id', async (req, res) => {
   const user_id = req.params.user_id;
   try {
     const user = await User.findOne({
-      attributes: ["id", "user_id", "name", "status_msg", "profile_img_url", "background_img_url"],
-      where: { user_id },
+      attributes: [
+        'id',
+        'user_id',
+        'name',
+        'status_msg',
+        'profile_img_url',
+        'background_img_url'
+      ],
+      where: { user_id }
     });
     if (user) {
-      return res.json({ 
+      return res.json({
         data: user,
-        msg: "이미 사용중이거나 탈퇴한 아이디입니다." 
+        msg: '이미 사용중이거나 탈퇴한 아이디입니다.'
       });
     }
     return res.json({
       data: null,
-      msg: "사용 가능한 ID 입니다.",
+      msg: '사용 가능한 ID 입니다.'
     });
   } catch (err) {
     return res.status(500).json({
-      msg: "서버 문제로 인해 찾을 수 없습니다."
-    })
+      msg: '서버 문제로 인해 찾을 수 없습니다.'
+    });
   }
 });
 
 // uid로 유저 정보를 찾습니다.
-router.get("/find/:id", async(req, res) => {
+router.get('/find/:id', async (req, res) => {
   const id = req.params.id;
   try {
     const user = await User.findOne({
-      attributes: ["id", "user_id", "name", "status_msg", "profile_img_url", "background_img_url"],
-      where: { id },
+      attributes: [
+        'id',
+        'user_id',
+        'name',
+        'status_msg',
+        'profile_img_url',
+        'background_img_url'
+      ],
+      where: { id }
     });
     return res.json({
       data: user,
-      msg: "사용자를 찾았습니다.",
+      msg: '사용자를 찾았습니다.'
     });
   } catch (err) {
     return res.status(500).json({
-      msg: "서버 문제로 인해 찾을 수 없습니다."
-    })
+      msg: '서버 문제로 인해 찾을 수 없습니다.'
+    });
   }
 });
 
-router.post("/profile/change", async(req, res) => {
+router.post('/profile/change', async (req, res) => {
   const body = req.body;
-  try{
-    await User.update({...body},
-    {
-        where: {id: body.id}
-    })
-    return res.json({data: true, msg: "프로필 변경 완료."})
-  }catch(err){
-    return res.status(400).json({data: false, msg: err.message});
-  }
-})
-
-router.post("/upload", upload.single("image"), (req, res) => {
   try {
-    const image = req.file;
+    await User.update(
+      { ...body },
+      {
+        where: { id: body.id }
+      }
+    );
+    return res.json({ data: true, msg: '프로필 변경 완료.' });
+  } catch (err: any) {
+    return res.status(400).json({ data: false, msg: err.message });
+  }
+});
+
+router.post('/upload', upload.single('image'), (req, res) => {
+  try {
+    const image = req.file as Express.Multer.File;
     return res.json({ data: `uploads\\${image.filename}` });
-  } catch (err) {
+  } catch (err: any) {
     return res.status(400).json({ msg: err.message });
   }
-})
+});
 
 export default router;
